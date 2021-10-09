49984224f00ca3682446794297baff8341139ba5
Divya <elataylor2002@gmail.com>
2021-10-09 12:28:21 +0530

    first commit

---
+++ past-events.js
@@ -0,0 +1,47 @@
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+
+var slideIndex = 1 ;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("past-card");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      
+      // slides[slideIndex+1].style.display = "block";
+      slides[slideIndex-1].style.display = "block";
+      slides[i].style.display = "none";
+      slides[slideIndex].style.display = "block";
+
+  }
+
+  
+  
+}
+
